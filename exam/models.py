c413056307fc06964e38048b0b49a51f4b8531ff
MatinMat8 <matinmat8.mat@gmail.com>
2022-01-31 20:19:20 +0330

    added Exam database

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+TYPE_OF_TEST = (
+    ('Descriptive', 'descriptive'),
+    ('Test', 'test'),
+    ('T/F', 't/f'),
+)
+
+
+class Exam(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    classroom = models.CharField(max_length=65)
+    exam_duration = models.IntegerField()
+    teacher_name = models.CharField(max_length=65)
+    type_of_exam = models.CharField(choices=TYPE_OF_TEST, max_length=20)
+    exam_url = models.URLField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    # students = models.ManyToManyField(student_model)
