85c6821d1bc906429911552a84abc3431353da65
MatinMat8 <matinmat8.mat@gmail.com>
2022-02-10 14:16:16 +0330

    handling add qs form

--- exam/forms.py
+++ exam/forms.py
@@ -1,8 +1,14 @@
 from django.forms.models import ModelForm
-from .models import Exam
+from .models import Exam, Question
 
 
 class AddExamForm(ModelForm):
     class Meta:
         model = Exam
         fields = ['title', 'classroom', 'exam_duration', 'teacher_name', 'type_of_exam', 'start_time', 'end_time']
+
+
+class AddQsForm(ModelForm):
+    class Meta:
+        model = Question
+        fields = ['question', 'option_1', 'option_2', 'option_3', 'option_4', 'qs_score', 'answer']
