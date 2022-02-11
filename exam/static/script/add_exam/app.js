4adfdb9f802ea351d8d25630d7abbddb403667a2
MatinNezami <matin912nezami@gmail.com>
2022-02-11 13:53:37 -0500

    add effect to input placeholder

---
+++ exam/static/script/add_exam/app.js
@@ -0,0 +1,25 @@
+function focus () {
+    this.parentNode.querySelector(".placeholder").classList.add("active");
+}
+
+function blur () {    
+    if (this.value)
+        return null;
+
+    this.parentNode.querySelector(".placeholder").classList.remove("active");
+}
+
+document.querySelectorAll(".input input").forEach(
+    input => {
+        input.addEventListener("focus", focus);
+        input.addEventListener("blur", blur);
+    }
+);
+
+function focusToInput () {
+    this.previousElementSibling.focus();
+}
+
+document.querySelectorAll(".placeholder").forEach(
+    placeholder => placeholder.addEventListener("click", focusToInput)
+);
